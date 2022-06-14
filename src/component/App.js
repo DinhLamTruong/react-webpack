2c97de9a0bade37399205fbb8bca06282af80100
truong <anhtrangdep012@gmail.cocm>
2022-06-14 22:29:30 +0700

    done

--- src/component/App.js
+++ src/component/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import Content from './Content';
 import "./App.css"
-import { useContext } from 'react'
-import { StoreContext } from '../store'
+import { useContext, useRef } from 'react'
+import { useStore, actions } from '../store'
+import { setTodoInput } from '../store/actions';
 
 
 function App() {
-    const [state, dispatch] = useContext(StoreContext)
-    console.log(state)
+    const InputRef = useRef()
+    const [state, dispatch] = useStore()
+    const { todos, todoInput } = state
+
+    const handleAdd = () => {
+        dispatch(actions.addTodo(todoInput))
+        dispatch(setTodoInput(''))
+        InputRef.current.focus()
+    }
+    console.log('todoInput:', todoInput)
     return (
         <div>
-            <input />
-            <button>Add</button>
+            <input
+                ref={InputRef}
+                value={todoInput}
+                placeholder="todo..."
+                onChange={e => {
+                    dispatch(actions.setTodoInput(e.target.value))
+                }}
+            />
+            <button onClick={handleAdd}>Add</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
         </div>
     )
 }
--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -1,10 +1,23 @@
+import { ADD_TODO_INPUT, SET_TODO_INPUT } from './constants'
+
 const initState = {
     todos: [],
     todoInput: ''
 }
 function reducer(state, action) {
-    switch (action) {
-
+    switch (action.type) {
+        case SET_TODO_INPUT:
+            return {
+                ...state,
+                todoInput: action.payload
+            }
+        case ADD_TODO_INPUT:
+            return {
+                ...state,
+                todos: [...state.todos, action.payload]
+            }
+        default:
+            throw Error('invalid actions')
     }
 }
 export { initState }
